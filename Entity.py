ba5075814284f3c54818a09a4b7e2ace831ec710
Jason Ye <jasonye4273@gmail.com>
2023-08-26 00:28:37 -0400

    spaces

--- Entity.py
+++ Entity.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-	from HexGrid import HexGrid, HexLocation
+    from HexGrid import HexGrid, HexLocation
 
 import pygame
 
